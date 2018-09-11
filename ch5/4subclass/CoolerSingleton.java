08e4b73736d83598c082a58bdb4a186c079ee46b
HuaTsai <a0910666734@gmail.com>
2018-09-11 17:59:51 +0800

    4subclass

---
+++ ch5/4subclass/CoolerSingleton.java
@@ -0,0 +1,7 @@
+public class CoolerSingleton extends Singleton {
+	protected static Singleton uniqueInstance;
+ 
+	private CoolerSingleton() {
+		super();
+	}
+}
---
+++ ch5/4subclass/SingletonClient.java
@@ -0,0 +1,8 @@
+public class SingletonClient {
+	public static void main(String[] args) {
+		Singleton foo = CoolerSingleton.getInstance();
+		Singleton bar = HotterSingleton.getInstance();
+		System.out.println(foo);
+		System.out.println(bar);
+ 	}
+}
